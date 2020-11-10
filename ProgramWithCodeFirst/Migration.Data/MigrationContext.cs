bc42feef50e5ad0ce8673e016358bceb938eb9b9
Nawiarra <nika.berezovskaya@ukr.net>
2020-11-11 00:59:47 +0200

    Set string fields length and create migration

--- ProgramWithCodeFirst/Migration.Data/MigrationContext.cs
+++ ProgramWithCodeFirst/Migration.Data/MigrationContext.cs
@@ -10,7 +10,7 @@ namespace Migration.Data
 {
     public class MigrationContext : DbContext
     {
-        public MigrationContext() : base("Data Source =(LocalDB)\\MSSQLLocalDB;Initial Catalog = ProgramWithCodeFirstDB;Integrated Security=true")
+        public MigrationContext() : base("Data Source = (LocalDB)\\MSSQLLocalDB;Initial Catalog = ProgramWithCodeFirstDB;Integrated Security=true")
         {
 
         }
@@ -31,9 +31,25 @@ namespace Migration.Data
                 .HasKey(x=>x.Id);
 
             modelBuilder.Entity<WoodFurnitureOrder>()
-            .Property(x => x.FullName)
-            .IsRequired()
-            .HasMaxLength(50);
+                .Property(x => x.FullName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<WoodFurnitureOrder>()
+                .Property(x => x.PhoneNumber)
+                .IsRequired()
+                .HasMaxLength(15);
+
+            modelBuilder.Entity<WoodFurnitureOrder>()
+                .Property(x => x.Color)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            modelBuilder.Entity<WoodFurnitureOrder>()
+                .Property(x => x.Date)
+                .IsRequired()
+                .HasMaxLength(20);
+
 
             #endregion
 
@@ -48,6 +64,16 @@ namespace Migration.Data
                 .WithRequired(x => x.FurnitureType)
                 .HasForeignKey(x => x.FurnitureTypeId);
 
+            modelBuilder.Entity<FurnitureType>()
+                .Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            modelBuilder.Entity<FurnitureType>()
+                .Property(x => x.Size)
+                .IsRequired()
+                .HasMaxLength(20);
+
             #endregion
 
             #region WoodType
@@ -60,6 +86,17 @@ namespace Migration.Data
                 .HasMany(x => x.WoodFurnitureOrders)
                 .WithRequired(x => x.WoodType)
                 .HasForeignKey(x => x.WoodTypeId);
+
+            modelBuilder.Entity<WoodType>()
+                .Property(x => x.TypeName)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            modelBuilder.Entity<WoodType>()
+                .Property(x => x.Price)
+                .IsRequired()
+                .HasMaxLength(15);
+
 
             #endregion
 
